2747e55cf7787f1221cd9ebcd41011eb4fc8673f
Tasha1003Natasha <0955712824n@gmail.com>
2022-06-28 11:27:13 +0300

    api ключ

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,14 +1,18 @@
 
-const baseUrl = 'https://restcountries.com/v2/name/';
-const fieldsUrl = 'fields=name,capital,population,flags,languages';
+// const baseUrl = 'https://restcountries.com/v2/name/';
+// const fieldsUrl = 'fields=name,capital,population,flags,languages';
 
 
-export function fetchCountries(name) {
-  return fetch(`${baseUrl}${name}?${fieldsUrl}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
+// export function fetchCountries(name) {
+//   return fetch(`${baseUrl}${name}?${fieldsUrl}`).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
 
-    return response.json();
-  });
-}
+//     return response.json();
+//   });
+// }
+
+
+// https://pixabay.com/api/
+// ключ (обязательно)	ул	Ваш ключ API:28317427-cd386f88f666cbda8176ce58f
