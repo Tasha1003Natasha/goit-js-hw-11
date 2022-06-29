fb0ccf5da7a123962aff17200c9e3ddde084408c
Tasha1003Natasha <102981670+Tasha1003Natasha@users.noreply.github.com>
2022-06-29 20:41:05 +0000

    Deploying to gh-pages from @ Tasha1003Natasha/goit-js-hw-11@7f01e77f9f392c7ae2d4d5d3082b64d690cfc640 🚀

--- index.508b57da.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#search-form");document.querySelector("button"),document.querySelector(".gallery");e.addEventListener("submit",(function(e){e.preventDefault();const{elements:{searchQuery:t}}=e.currentTarget;t.value;e.currentTarget.reset()}));
-//# sourceMappingURL=index.508b57da.js.map
--- index.ce3e00ef.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("#search-form");document.querySelector("button"),document.querySelector(".gallery");e.addEventListener("submit",(function(e){e.preventDefault();e.currentTarget.elements.searchQuery.value;e.currentTarget.reset()}))}();
-//# sourceMappingURL=index.ce3e00ef.js.map
